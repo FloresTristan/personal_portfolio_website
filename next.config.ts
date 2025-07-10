0e423a3c2b81a342609d5e825b3c1b423ce074a8
FloresTristan <147235296+FloresTristan@users.noreply.github.com>
2025-07-10 09:07:21 +0800

    .

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  
-  // webpack(config) {
-  //   return config;
-  // },
+  webpack: (config) => config
 };
 
 export default nextConfig;
